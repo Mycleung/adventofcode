487c9dfdee66210b2aada2c21208b0b1fea0e4e6
Michael Leung <solaris999@gmail.com>
2017-12-02 18:59:25 +0000

    Day1 complete, starting Day2

--- adventofcode/src/test/scala/day1/Day1Spec.scala
+++ adventofcode/src/test/scala/day1/Day1Spec.scala
@@ -3,7 +3,7 @@ package day1
 import org.scalatest._
 
 class Day1Spec extends WordSpec with Matchers {
-  "Day1 Inverse Captcha" should {
+  "Day1 Inverse Captcha 1" should {
     "get 3 for 1122" in {
       Day1.inverseCaptchaNext("1122") shouldBe 3
     }
@@ -22,6 +22,28 @@ class Day1Spec extends WordSpec with Matchers {
 
     "get 0 for 5" in {
       Day1.inverseCaptchaNext("5") shouldBe 0
+    }
+  }
+
+  "Day1 Inverse Captcha 2" should {
+    "get 6 for 1212" in {
+      Day1.inverseCaptchaHalf("1212") shouldBe 6
+    }
+
+    "get 0 for 1221" in {
+      Day1.inverseCaptchaHalf("1221") shouldBe 0
+    }
+
+    "get 4 for 123425" in {
+      Day1.inverseCaptchaHalf("123425") shouldBe 4
+    }
+
+    "get 12 for 123123" in {
+      Day1.inverseCaptchaHalf("123123") shouldBe 12
+    }
+
+    "get 4 for 12131415" in {
+      Day1.inverseCaptchaHalf("12131415") shouldBe 4
     }
   }
 }
