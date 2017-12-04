86e7c8cc555eb5a7ec422f38be7530c979656319
Michael Leung <solaris999@gmail.com>
2017-12-04 16:05:26 +0000

    Day 2 complete, starting day 3

---
+++ adventofcode/src/test/scala/day2/Day2Test.scala
@@ -0,0 +1,25 @@
+package day2
+
+import org.scalatest.{Matchers, WordSpec}
+
+class Day2Test extends WordSpec with Matchers {
+
+  "day2 part 1" should {
+    "calculate a checksum" in {
+      val test = """5	1	9	5
+                   |7	5	3
+                   |2c4	6	8""".stripMargin
+      Day2.solve1(test) shouldBe 18
+    }
+  }
+
+  "day2 part 2" should {
+    "calculate a checksum" in {
+      val test = """5	9	2	8
+                   |9	4	7	3
+                   |3	8	6	5""".stripMargin
+      Day2.solve2(test) shouldBe 9
+    }
+  }
+
+}
